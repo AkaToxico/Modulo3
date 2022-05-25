e50a7af3704814e8c2f1c6ecd6a01d94c09ff8d7
Luis Montero <luisemontero16@gmail.com>
2022-05-25 18:12:41 -0400

    Evaluacion

--- M03C09/js/Ejercicio_Evaluativo.js
+++ M03C09/js/Ejercicio_Evaluativo.js
@@ -1,5 +1,5 @@
 var numberOfSquares = 6;
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numberOfSquares);
 var square = document.querySelectorAll(".square");
 var message = document.querySelector("#message");
 var titulo = document.querySelector("h1");
@@ -9,7 +9,7 @@ var reset = document.querySelector(".reset");
 var dificil = document.querySelector("#dificil");
 var facil = document.querySelector("#facil");
 
-
+//Funciones----------------------------------------------------------------------------------------------------
 
 function cuadrados(){                                     //Recorre los cuadrados y les da color de fondo.
   for (i = 0; i < square.length; i++) {
@@ -47,12 +47,9 @@ function pickColor(num){                                        //Numero aleator
     return Math.ceil(Math.random()*num-1).toFixed(0);
 }
 
-
 function randomColor(){                                        //color aleatorio
     return "rgb("+pickColor(255)+", "+pickColor(255)+", "+pickColor(255)+")";
 }
-
-//---------------------------------
 
 function generateRandomColors(numberOfSquares){
   if(numberOfSquares==6){
@@ -61,31 +58,35 @@ function generateRandomColors(numberOfSquares){
     return [randomColor(),randomColor(),randomColor()];
   }
 }
-//var numberOfSquares = 6
-generateRandomColors(numberOfSquares)
-//console.log(generateRandomColors(numberOfSquares))
-//----------------------------------
 
-function generateRandomColors6(){                             // 6 colores
-   return [randomColor(),randomColor(),randomColor(),randomColor(),randomColor(),randomColor()];
-}
-
-function generateRandomColors3(){                            //3 colores
-    return [randomColor(),randomColor(),randomColor()];
-}
-
-
+//Eventos-----------------------------------------------------------------------------------------------------
  
 reset.addEventListener("click", function(){                 // Nuevos colores
-  colors = generateRandomColors(numberOfSquares);
-  pickedColor = colors[pickColor(5)];
-  colorDisplay.innerHTML = pickedColor;
-  reset.innerHTML="Nuevos Colores"
-  message.innerHTML="";
-  facil.classList.remove("selected");
-  dificil.classList.add("selected")
-  juego();
-  
+ // colors = generateRandomColors(numberOfSquares);
+
+  if (facil.classList.contains("selected")){
+    numberOfSquares = 3;
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = colors[pickColor(3)];
+    colorDisplay.innerHTML = pickedColor;
+    reset.innerHTML="Nuevos Colores"
+    message.innerHTML="";
+    juego()
+    for(i=3; i<=5; i++){
+      square[i].style.visibility="hidden"; 
+    }
+    
+  } else{
+    numberOfSquares = 6;
+    colors=generateRandomColors(numberOfSquares);
+    pickedColor = colors[pickColor(6)];
+    colorDisplay.innerHTML = pickedColor;
+    reset.innerHTML="Nuevos Colores"
+    message.innerHTML="";
+    for (i = 0; i < square.length; i++) {
+      square[i].style = "background-color:"+colors[i];
+    }
+  }  
 })
 
 dificil.addEventListener("click", function(){               //modo de juego dificil
@@ -93,7 +94,7 @@ dificil.addEventListener("click", function(){               //modo de juego difi
   dificil.classList.add("selected")
   numberOfSquares = 6;
   colors=generateRandomColors(numberOfSquares);
-  pickedColor = colors[pickColor(5)];
+  pickedColor = colors[pickColor(6)];
   colorDisplay.innerHTML = pickedColor;
   reset.innerHTML="Nuevos Colores"
   message.innerHTML="";
@@ -102,26 +103,18 @@ dificil.addEventListener("click", function(){               //modo de juego difi
   }
 })
 
-
 facil.addEventListener("click", function(){                 //modo de juego facil
   facil.classList.add("selected");
   dificil.classList.remove("selected")
   numberOfSquares = 3;
   colors = generateRandomColors(numberOfSquares);
-  pickedColor = colors[pickColor(2)];
+  pickedColor = colors[pickColor(3)];
   colorDisplay.innerHTML = pickedColor;
   reset.innerHTML="Nuevos Colores"
   message.innerHTML="";
     juego()
   for(i=3; i<=5; i++){
-    square[i].style.visibility="hidden";
-  //square[0].style.backgroundColor="pickedColor[0]";
-  ///square[1].style.backgroundColor="pickedColor[1]";
-  //square[2].style.backgroundColor="pickedColor[2]";
- // square[3].style.visibility="hidden";
- // square[4].style.visibility = "hidden";
- // square[5].style.visibility = "hidden";
-  
+    square[i].style.visibility="hidden"; 
   }
 })
 
